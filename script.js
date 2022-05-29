088bae0180fb9fba498acb79f173e0bdef922c2d
ET45 <emre.tugutlu@gmail.com>
2022-05-29 14:14:06 +0300

    creating paddles

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-import Ball from "./Ball.js";
+import Ball from "./Ball";
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 let lastTime;
 function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime;
-    ball.update(delta);
+    /* ball.update(delta); */
   }
 
   lastTime = time;
