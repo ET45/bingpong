bb0b05aba1adf02833f89c44ac6570cbb735d640
ET45 <emre.tugutlu@gmail.com>
2022-05-31 15:01:30 +0300

    reset added

--- Paddle.js
+++ Paddle.js
@@ -3,6 +3,7 @@ const SPEED = 0.02;
 export default class Paddle {
   constructor(paddleElem) {
     this.paddleElem = paddleElem;
+    this.reset();
   }
   get position() {
     return parseFloat(
@@ -12,6 +13,10 @@ export default class Paddle {
 
   set position(value) {
     this.paddleElem.style.setProperty("--position", value);
+  }
+
+  reset() {
+    this.position = 50;
   }
 
   update(delta, ballHeight) {
